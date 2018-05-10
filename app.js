ca90971289c50923d04d8418b14e858596d29238
thisismrsanjay <thisismrsanjay@gmail.com>
2018-05-10 08:54:33 +0530

    handeled auth

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const experss = require('express');
 const app = experss();
 const mongoose = require('mongoose');
 const port = process.env.PORT || 3000;
+const passport = require('passport');
+require('./config/passport')(passport);
+
+const auth = require('./routes/auth');
 
 app.get('/',(req,res)=>{
     res.send('It works');
 })
+app.use('/auth',auth);
+
 
 app.listen(port,()=>{
 console.log(`Server Started ${port}`)
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports ={
+    mongoURI:'mongodb://sanjay:sanjay@ds247569.mlab.com:47569/bit-dairies',
+    googleClientID:'240050420505-4ptftbvkkhfbd7ft58vm861gaal6vvtd.apps.googleusercontent.com',
+    googleClientSecret:'cIHQ48qC7eM9wNfgop4_8SAz'
+}
